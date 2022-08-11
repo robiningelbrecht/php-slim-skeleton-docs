4f3a9ecfb2ecce71d533519dd8fca65b83905782
robiningelbrecht <robin@baldwin.be>
2022-08-11 08:19:58 +0200

    url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'slim-skeleton-ddd-amqp-docs.vercel.app',
+  url: 'https://slim-skeleton-ddd-amqp-docs.vercel.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
